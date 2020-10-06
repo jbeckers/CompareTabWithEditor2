d575efb5b978c7b6cc7451ab847d0cf9b99d3903
Joachim Beckers <joachim.beckers@uzleuven.be>
2020-10-06 07:38:49 +0200

    Version 1.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,17 @@
 import org.jetbrains.changelog.closure
+import org.jetbrains.changelog.date
 
 group = "be.jbeckers"
-version = "1.0.3"
+version = "1.0.4"
 
 repositories {
     mavenCentral()
 }
 
 plugins {
-    id("org.jetbrains.intellij") version "0.4.21"
-    id("org.jetbrains.changelog") version "0.1.5"
-    kotlin("jvm") version "1.3.72"
+    id("org.jetbrains.intellij") version "0.5.0"
+    id("org.jetbrains.changelog") version "0.5.0"
+    id("org.jetbrains.kotlin.jvm") version "1.4.10"
 }
 
 intellij {
@@ -21,7 +22,7 @@ tasks {
     patchPluginXml {
         pluginId("be.jbeckers.compare_tab_with_editor2")
         pluginDescription("Compare the currently open file to another one, by right click on editor tab.")
-        changeNotes(closure { changelog.get("${project.version}").noHeader().toHTML() })
+        changeNotes(closure { changelog.get("${project.version}").toHTML() })
         version("${project.version}")
         sinceBuild("192")
     }
@@ -32,9 +33,5 @@ tasks {
 }
 
 changelog {
-    version = "${project.version}"
     path = "${project.projectDir}/docs/CHANGELOG.md"
-    format = "[{0}]"
-    keepUnreleasedSection = true
-    unreleasedTerm = "Unreleased"
 }
--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -6,7 +6,13 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 ### Changed
-- Use gradle-changelog-plugin
+
+## [1.0.4]
+### Changed
+- Support IntelliJ 2020.3 EAP
+- Update to Gradle 6.6.1
+- Update to gradle-intellij-plugin 0.5.0
+- Use gradle-changelog-plugin  0.5.0
 
 ## [1.0.3]
 ### Changed
@@ -30,6 +36,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Updated for Intellij 2019.2+, since some deprecated APIs were removed
 
 [Unreleased]: https://github.com/jbeckers/CompareTabWithEditor2/compare/1.0.3...HEAD
+[1.0.4]: https://github.com/jbeckers/CompareTabWithEditor2/compare/1.0.3...1.0.4
 [1.0.3]: https://github.com/jbeckers/CompareTabWithEditor2/compare/1.0.2...1.0.3
 [1.0.2]: https://github.com/jbeckers/CompareTabWithEditor2/compare/1.0.1...1.0.2
 [1.0.1]: https://github.com/jbeckers/CompareTabWithEditor2/compare/1.0.0...1.0.1
