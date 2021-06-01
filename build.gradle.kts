1e5818344cb4d7d56e24588a51cbf69dd08f1517
Joachim Beckers <joachim.beckers@uzleuven.be>
2021-06-01 09:24:39 +0200

    Fix more klint errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 import io.gitlab.arturbosch.detekt.Detekt
-import org.jetbrains.changelog.closure
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
