5c4db05047b0815101cf08b3f460fdda3afe538d
Joachim Beckers <joachim.beckers@uzleuven.be>
2022-08-02 11:19:53 +0200

    Move FileEditorManagerListener to its own class, and register this class in plugin.xml

--- src/main/kotlin/be/jbeckers/ctwe/CompareTabWithEditor.kt
+++ src/main/kotlin/be/jbeckers/ctwe/CompareTabWithEditor.kt
@@ -7,11 +7,13 @@ import com.intellij.diff.requests.SimpleDiffRequest
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys.VIRTUAL_FILE
 import com.intellij.openapi.diff.DiffBundle
-import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
+import com.intellij.openapi.fileEditor.FileEditorManagerEvent
+import com.intellij.openapi.fileEditor.FileEditorManagerListener
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.VirtualFile
 
-class CompareTabWithEditor : CompareFilesAction() {
+class CompareTabWithEditorAction : CompareFilesAction() {
 
     @Suppress("DialogTitleCapitalization")
     override fun getDiffRequest(e: AnActionEvent): DiffRequest? {
@@ -21,7 +23,7 @@ class CompareTabWithEditor : CompareFilesAction() {
             val project = e.project ?: return null
 
             val left = e.getData(VIRTUAL_FILE)
-            val right = CompareTabWithEditorService.getInstance(project).currentFile
+            val right = project.getService(CompareTabWithEditorService::class.java).currentFile
             if (left == null || right == null) {
                 null
             } else {
@@ -47,19 +49,36 @@ class CompareTabWithEditor : CompareFilesAction() {
             presentation.isVisible = false
         } else {
             val left = e.getData(VIRTUAL_FILE)
-            val right = CompareTabWithEditorService.getInstance(project).currentFile
+            val right = project.getService(CompareTabWithEditorService::class.java).currentFile
             presentation.isVisible = left != null && right != null && left != right
         }
     }
 
-    companion object {
+    private fun getVirtualFileContentTitle(documentFile: VirtualFile): String {
+        val name = documentFile.name
+        val parent = documentFile.parent
+        return if (parent != null) {
+            name + " (" + FileUtil.toSystemDependentName(parent.path) + ")"
+        } else name
+    }
+}
 
-        private fun getVirtualFileContentTitle(documentFile: VirtualFile): String {
-            val name = documentFile.name
-            val parent = documentFile.parent
-            return if (parent != null) {
-                name + " (" + FileUtil.toSystemDependentName(parent.path) + ")"
-            } else name
-        }
+class CompareTabWithEditorService {
+
+    @Volatile
+    var currentFile: VirtualFile? = null
+        internal set
+}
+
+class CompareTabWithEditorListener : FileEditorManagerListener {
+
+    private val project: Project
+
+    constructor(project: Project) {
+        this.project = project
+    }
+
+    override fun selectionChanged(event: FileEditorManagerEvent) {
+        project.getService(CompareTabWithEditorService::class.java).currentFile = event.newFile
     }
 }
