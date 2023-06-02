bec97cc1f574e5e6a84eec1ad3f4661102622f91
Joachim Beckers <joachim.beckers@nexuzhealth.com>
2023-06-02 18:44:10 +0200

    Migrate to kover 0.7+

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,8 +56,12 @@ qodana {
 }
 
 // Configure Gradle Kover Plugin - read more: https://github.com/Kotlin/kotlinx-kover#configuration
-kover.xmlReport {
-    onCheck.set(true)
+koverReport {
+    defaults {
+        xml {
+            onCheck = true
+        }
+    }
 }
 
 tasks {
