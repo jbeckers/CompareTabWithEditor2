2e307f415d43b89c9b33cbc4dcf97d3fe8609bdb
Joachim Beckers <joachim.beckers@nexuzhealth.com>
2026-09-09 15:11:54 +0200

    Target 2025.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-        intellijIdea("2025.1.5")
+        intellijIdea("2025.1")
 //        testFramework(TestFrameworkType.Platform)
     }
 }
